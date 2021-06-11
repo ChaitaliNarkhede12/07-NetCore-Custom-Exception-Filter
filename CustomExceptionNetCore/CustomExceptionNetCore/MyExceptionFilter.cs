2c562dd1d77a6329f9be8da6958a54e7f27c93a9
chaitali-narkhede <chairali.narkhede1991@gmail.com>
2021-06-11 20:47:38 +0530

    check in

---
+++ CustomExceptionNetCore/CustomExceptionNetCore/MyExceptionFilter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CustomExceptionNetCore
+{
+    public class MyExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            HttpStatusCode status = HttpStatusCode.InternalServerError;
+            var message = "Server error occurred.";
+
+            var exceptionType = context.Exception.GetType();
+
+            message = context.Exception.Message;
+
+
+            //You can enable logging error
+
+            context.ExceptionHandled = true;
+            HttpResponse response = context.HttpContext.Response;
+            response.StatusCode = (int)status;
+            response.ContentType = "application/json";
+            context.Result = new ObjectResult(new { Message = message});
+        }
+    }
+}
